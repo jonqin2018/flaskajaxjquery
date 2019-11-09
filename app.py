0618510382c81dfd7a2e9599930043fd5b47b8f1
chuan <chuan@example.com>
2019-11-08 23:04:59 -0700

    working

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import os
 from flask import Flask,render_template, request,json,jsonify
-
+from flask import Blueprint, render_template, redirect, url_for, request, flash, session, send_file, send_from_directory
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Welcome to Python Flask!'
+@app.route('/result')
+def result():
+    data = request.args.get('data')
+    data_1 = [].append(json.loads(data))
+    print(data_1)
+    return data
 
 @app.route('/signup')
 def signUp():
@@ -16,14 +19,14 @@ def signUp():
            }
     return render_template('signUp.html', data=data)
 
-@app.route('/signupuser', methods=['POST'])
-def signupuser():
-    test = request.args.get('mydata')
-    print(test)
-    print("something is not right")
-    # user =  request.form['username'];
-    # password = request.form['password'];
-    return json.dumps({'status':'200'})
+# @app.route('/signupuser', methods=['POST'])
+# def signupuser():
+#     # test = request.args.get('mydata')
+#     # print(test)
+#     # print("something is not right")
+#     # # user =  request.form['username'];
+#     # # password = request.form['password'];
+#     return json.dumps({'status':'200!'})
 
 if __name__=="__main__":
     app.run(debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,46 @@
 
-$(function(){
-	$('button').click(function(){
-		$.ajax({
-			url: '/signupuser',
-			data:{"mydata": "world"},
-			type: 'POST',
-			success: function(response){
-				console.log(response);
-				alert("success");
-			},
-			error: function(error){
-				console.log(error);
-			}
-		});
-	});
-});
+// $(function(){
+// 	$('button').click(function(){
+// 		$.ajax({
+// 			url: '/signupuser',
+// 			data:{"mydata": "world"},
+// 			type: 'POST',
+// 			success: function(response){
+// 				console.log(response);
+// 				alert("success");
+// 			},
+// 			error: function(error){
+// 				console.log(error);
+// 			}
+// 		});
+// 	});
+// });
 
+function CreateTable(rowCount,cellCount)
+{
+  $("#createtable").empty();
+   var table=$("<table border='1' cellspacing='0'> </table>");
+   table.appendTo($("#createtable"));
+
+   var trh=$("<tr></tr>");
+   trh.appendTo(table);
+
+   for(var h=0; h<cellCount; h++)
+   {
+	  var th = $("<th>" +h+"</th>");
+	  th.appendTo(trh);
+   }
+
+   for(var i=0;i<rowCount;i++)
+   {
+	  var tr=$("<tr></tr>");
+	  tr.appendTo(table);
+	  for(var j=0;j<cellCount;j++)
+	  {
+		 var td=$("<td>"+i*j+"</td>");
+		 td.appendTo(tr);
+	  }
+   }
+
+}
 
