12ef5b72dc68d43924a6feccbc94e29a36f52899
chuan <chuan@example.com>
2019-11-09 23:14:23 -0700

    beautify page

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import os
-from flask import Flask,render_template, request,json,jsonify
+from flask import Flask,render_template, request,json,jsonify, session
 from flask import Blueprint, render_template, redirect, url_for, request, flash, session, send_file, send_from_directory
 
 app = Flask(__name__)
+
+
+app.config['SECRET_KEY'] = 'the random string' 
 
 @app.route('/result')
 def result():
@@ -11,8 +14,8 @@ def result():
     # print(data_1)
     return data
 
-@app.route('/signup')
-def signUp():
+@app.route('/')
+def first_page():
     data = [ 
             {"IP":"1.1.1.1",
              "CONFIG1": "blah",
@@ -30,7 +33,12 @@ def signUp():
             }
 
            ]
-    return render_template('signUp.html', data=data)
+    string_var = "This is a string"
+
+    session["cli_username"] = "cli_username_value"
+    session["cli_password"] = "cli_password_value"
+
+    return render_template('apphome.html', data=data, string_var=string_var)
 
 if __name__=="__main__":
     app.run(debug=True)
