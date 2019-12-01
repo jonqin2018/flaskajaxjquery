efcda786609f6174b99669edcd16382522ee1cfc
chuan <chuan@example.com>
2019-11-30 21:29:06 -0700

    add dropdown

--- templates/test.js
+++ templates/test.js
@@ -1,7 +1,14 @@
 describe("pow", function() {
+    describe("pow", function() {
 
-    it("raises to n-th power", function() {
-      assert.equal(pow(2, 3), 8);
-    });
+        it("2 raised to power 3 is 8", function() {
+          assert.equal(pow(2, 3), 8);
+        });
+      
+        it("3 raised to power 3 is 27", function() {
+          assert.equal(pow(3, 3), 27);
+        });
+      
+      });
   
   });
